7493bd3b6782e26f07cefda13bffcc60f2dca833
Aadi Dapurkar <aadidapurkar04@gmail.com>
2026-02-12 02:18:21 +1100

    improved tab lock feature - narrowed whitelist down further to only exclude extn url, which fixes new tab bypass/cheat, currently the only cheat to be resolved from testing is hold mouse1 to move away from a locked tab (probably because of the jerry rig 10 try switch back to enforce the tab lock)

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -20,7 +20,7 @@ const handleInitial = async () => {
 handleInitial();
 
 // EVENT LISTENER - Enforce tab limit
-chrome.tabs.onCreated.addListener(async _ => {
+chrome.tabs.onCreated.addListener(async info => {
     const state = await getState();
 
     const tabCount = await getTabCount();
@@ -31,6 +31,15 @@ chrome.tabs.onCreated.addListener(async _ => {
 
         chrome.tabs.remove(tabsToClose.map(tab => tab.id!));
     }
+
+    const tabData = await chrome.tabs.get(info.id!);
+    const state2 = await getState();
+    const exists = await stateExists();
+    if (!exists) {
+        await chrome.storage.local.set(defaultStorage);
+    }
+    const extnOrigin = `chrome-extension://${chrome.runtime.id}`;
+    enforceLock(tabData, info, state, extnOrigin)
 });
 
 // EVENT LISTENER - Enforce site bans
@@ -55,20 +64,29 @@ chrome.tabs.onActivated.addListener(async info => {
     if (!exists) {
         await chrome.storage.local.set(defaultStorage);
     }
+    // each of the below condition tries to validate that the tab just activated/created is a 'naughty' tab
+    // if any of the conditions fail then the tab lock integrity is fine and we dont need to do whats in the innermost condition (switch back)
 
-    // Condition 0 - The URL is not a chrome:// URL
-    if (tabData.url && !tabData.url.startsWith("chrome://")) {
-        // Condition 1 - There has been a lock previously set for this window
-        if (info.windowId in state.lock) {
+    const extnOrigin = `chrome-extension://${chrome.runtime.id}`;
+    enforceLock(tabData, info, state, extnOrigin)
+});
+
+
+const enforceLock = async (tabData : chrome.tabs.Tab, info : chrome.tabs.OnActivatedInfo | chrome.tabs.Tab, state : State, extnOrigin : string) => {
+    // Condition 0 - The URL does not belong to the extension itself (need to whitelist extn so user can turn tab lock off)
+    if (tabData.url && !tabData.url.startsWith(extnOrigin)) {
+        const currentTabId = 'tabId' in info ? info.tabId : info.id;
+        // Condition 1 - There has been a lock previously set for this window and is currently set
+        if (info.windowId in state.lock && state.lock[info.windowId][0] === true) {
             console.log("Condition 1 met");
+
             // Condition 2 - The activated tab is not the locked tab, and the window is locked
-            if (
-                info.tabId !== state.lock[info.windowId][1] &&
-                state.lock[info.windowId][0] === true
-            ) {
+            if (currentTabId !== state.lock[info.windowId][1]) {
                 console.log("Condition 2 met");
                 const lockedTabId = state.lock[info.windowId][1];
+                
 
+                // DETECTED that the current tab is naughty
                 // Retry switching back sequentially up to 10 times
                 for (let i = 0; i < 10; i++) {
                     const tbs = await chrome.tabs.query({
@@ -90,4 +108,4 @@ chrome.tabs.onActivated.addListener(async info => {
             }
         }
     }
-});
+}
